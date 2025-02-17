ceab599426d116ab4b989ec099d5b9450c88d02a
Marsziii <kovmar138@hengersor.hu>
2025-02-17 11:29:08 +0100

    szükséges modulok importááálása, importálása
    bootstrap route tachyons

---
+++ src/oldalak/Ermesek.jsx
@@ -0,0 +1,16 @@
+import React from "react";  
+
+const Ermesek=(()=>{
+
+    return(
+        <div>   
+            <h1>Az olimpia érmesei
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, tempora numquam inventore eaque, dicta cumque possimus iste, provident adipisci facilis autem nisi quas! Rerum dignissimos, repudiandae esse dolorum expedita possimus, architecto, sit tenetur exercitationem ratione sint aut minima modi quo. Quos tempora eveniet sapiente nobis recusandae itaque cupiditate rem tempore quis minus similique dolorem dolor sequi doloribus placeat esse perferendis, corrupti corporis beatae voluptatum omnis. Blanditiis labore debitis voluptatum illo optio cupiditate fugiat vero non sit aut aliquid dignissimos minus earum, accusantium temporibus sequi quo velit dolorum architecto ipsa quae, reiciendis repellat et! Culpa vel, ex dolorem sunt laudantium quae?</p>
+        </div>
+    )
+
+
+})
+
+export default Ermesek
