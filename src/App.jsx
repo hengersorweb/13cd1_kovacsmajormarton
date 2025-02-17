018ca914dc5416cce277c8f5865afbe903ab0202
Marsziii <kovmar138@hengersor.hu>
2025-02-17 11:06:33 +0100

    alaprendszer beállítása
    bootstrap router és a tachyons modul letöltése
    app.jsx és a

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>covid</h1>
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
